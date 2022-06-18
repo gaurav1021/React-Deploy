b80ecf458fd70adc2e94cdee567aa171697bca36
GAURAV MANGAL <gauravmangal389@gmail.com>
2022-06-18 19:38:41 +0530

    Updates

---
+++ precache-manifest.dc253f9ae0f2b551de056a6b9454e294.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63cbad3cc30b0d50ebf7ab4457827fbb",
+    "url": "/React-Deploy/index.html"
+  },
+  {
+    "revision": "91dcddfb80aa5ef12447",
+    "url": "/React-Deploy/static/js/2.32b9d141.chunk.js"
+  },
+  {
+    "revision": "5224d6f0e6272d4f11ed",
+    "url": "/React-Deploy/static/js/main.09ba77c5.chunk.js"
+  },
+  {
+    "revision": "cad27ca70989ad40bf07",
+    "url": "/React-Deploy/static/js/runtime-main.284cffb6.js"
+  }
+]);
